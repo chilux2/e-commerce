d07eee026b5ee3cdd85e5ecdaa3abd42621e3cca
chilux2 <chilu.chilufya@hotmail.co.uk>
2023-08-22 17:53:33 +0100

    config changed

--- config.js
+++ config.js
@@ -1,12 +1,4 @@
 module.exports = {
   PORT: process.env.PORT,
-    DB: {
-      DB_HOST: process.env.DB_HOST,
-      DB_USER: process.env.DB_USER,
-      DB_DATABASE: process.env.DB_DATABASE,
-      DB_POST: process.env.DB_POST,
-      DB_PASSWORD: process.env.DB_PASSWORD,
-      DB_PORT: process.env.DB_PORT
-    },
-    SESSION_SECRET: process.env.SESSION_SECRET
+  SESSION_SECRET: process.env.SESSION_SECRET
   }
