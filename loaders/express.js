5655c06b93308a85be9ff80200e4f624ce2cd408
chilux2 <chilu.chilufya@hotmail.co.uk>
2024-07-25 14:53:24 +0100

    start again commit

--- loaders/express.js
+++ loaders/express.js
@@ -4,34 +4,3 @@ const session = require('express-session');
 const { SESSION_SECRET } = require('../config');
 require("dotenv").config();
 
-module.exports = (app) => {
-
-
-  app.use(cors());
-  //app.use(express.json());
-
-  app.use(bodyParser.json());
-
-
-  app.use(
-    bodyParser.urlencoded({
-      extended: true,
-    })
-  )
-
-
-app.use(
-    session({  
-      secret: process.env.SESSION_SECRET || 'session_secret',
-      //secret: SESSION_SECRET,
-      resave: false,
-      saveUninitialized: false,
-      cookie: {
-        secure: false,
-        maxAge: 24 * 60 * 60 * 1000
-      } 
-    })
-  );
-    return app;
-    
-}
--- services/userService.js
+++
@@ -1,8 +0,0 @@
-const pool = require('../index');
-
-module.exports = {
-    async getUser(username) {
-        const user = await pool.query('SELECT * FROM customers WHERE username = $1', [username]);
-        return user.rows[0];
-    }
-}
