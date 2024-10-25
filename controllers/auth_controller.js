63db431358988e554a32bbf8ac61b7ed8dab4a2e
chilux2 <chilu.chilufya@hotmail.co.uk>
2024-10-25 11:37:17 +0100

    auth changes

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -14,9 +14,6 @@ const registerUser = (req, res) => {
 
             pool.query(auth_query.registerUser, [first_name, last_name, customer_email, password, username ], (error, results) => {
                 if(error) {throw error}
-        
-                //res.status(201).json(`User successfully created with ID: ${results.rows[0]}`);
-                //res.status(201).json(`User successfully created with ID: ${results.rows[0].customer_email}`);
                 res.status(201).send('User created successfully');
             })
         }
@@ -24,47 +21,9 @@ const registerUser = (req, res) => {
 
 }
 
-   /*const getCustomerEmail = (username, password, done) => {
-        pool.query(auth_query.getCustomerEmail, [username], async (error, results) => {
-
-            if(!username) {
-                res.status(401).send('Incorrect username or password');
-            };
-            
-            if(username.password !== password) {
-                res.status(401).send('Incorrect username or password');
-            };
-    
-            res.status(201).send('Login successful');
-            done();
-        });
-    }*/ 
-
-/*
-    const getCustomerEmail = (req, res) => {
-        
-        const { first_name, last_name, customer_email, password, username } = req.body;
-
-        pool.query(auth_query.getCustomerEmail, [customer_email], (password,username) => {
-
-            if(!username) {
-                res.status(401).send('Incorrect username or password');
-            };
-            
-            if(username.password !== password) {
-                res.status(401).send('Incorrect username or password');
-            };
-    
-            res.status(201).send('Login successful');
-        })
-    ;
-    } 
-
-//npm run devStart
- */
 
 module.exports = {
     registerUser,
-    //getCustomerEmail,
+    
     
 }
